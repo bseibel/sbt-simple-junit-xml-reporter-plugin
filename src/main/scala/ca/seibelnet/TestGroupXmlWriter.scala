8644c7831ac93ac59b542d6ce7a957c4f5162214
David R <d.riley@wintoncapital.com>
2013-10-04 13:55:34 +0100

    Updated for sbt 0.13.0. Incremented version to 0.3

--- src/main/scala/ca/seibelnet/TestGroupXmlWriter.scala
+++ src/main/scala/ca/seibelnet/TestGroupXmlWriter.scala
@@ -1,13 +1,14 @@
 package ca.seibelnet
 
 import collection.mutable.ListBuffer
-import sbt.TestEvent
+import sbt.{ TestEvent, TestResult }
+import sbt.testing.Event
 import java.net.InetAddress
 import java.util.Date
-
 import xml.XML
-import org.scalatools.testing.{Result, Event}
+import org.scalatools.testing.Result
 import java.text.SimpleDateFormat
+import sbt.testing.TestSelector
 
 /**
  * User: bseibel
@@ -15,14 +16,12 @@ import java.text.SimpleDateFormat
  * Time: 2:01 PM
  */
 
-
 object TestGroupXmlWriter {
 
   def apply(name: String) = {
     new TestGroupXmlWriter(name)
   }
 }
-
 
 class TestGroupXmlWriter(val name: String) {
 
@@ -34,38 +33,32 @@ class TestGroupXmlWriter(val name: String) {
   lazy val hostName = InetAddress.getLocalHost.getHostName
   lazy val testEvents: ListBuffer[TestEvent] = new ListBuffer[TestEvent]
 
-
   def addEvent(testEvent: TestEvent) {
     testEvents += testEvent
     for (e: Event <- testEvent.detail) {
       tests += 1
-      e.result() match {
-        case Result.Failure => failures += 1
-        case Result.Error => errors += 1
-        case Result.Skipped => skipped += 1
+      e match {
+        case TestResult.Failed => failures += 1
+        case TestResult.Error => errors += 1
         case _ => {}
       }
     }
   }
 
-
   def write(path: String) {
 
     val resultXml =
-      <testsuite errors={errors.toString} failures={failures.toString} name={name} tests={tests.toString} time={"0"} timestamp={new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(new Date())}>
-          <properties/>
+      <testsuite errors={ errors.toString } failures={ failures.toString } name={ name } tests={ tests.toString } time={ "0" } timestamp={ new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(new Date()) }>
+        <properties/>
         {
-          for (e <- testEvents; t <- e.detail) yield
-          {
-            <testcase classname={name} name={ t.testName() } time={"0"}>
+          for (e <- testEvents; t <- e.detail) yield {
+            <testcase classname={ name } name={ t.selector.asInstanceOf[TestSelector].testName } time={ "0" }>
               {
-                t.result() match {
-                  case Result.Failure =>
-                    <failure message={t.error().getMessage} type={t.error().getClass.getName}>{t.error().getStackTrace.map { e => e.toString }.mkString("\n")}</failure>
-                  case Result.Error =>
-                    <error message={t.error().getMessage} type={t.error().getClass.getName}>{t.error().getStackTrace.map { e => e.toString }.mkString("\n")}</error>
-                  case Result.Skipped =>
-                    <skipped/>
+                t match {
+                  case TestResult.Failed =>
+                    <failure message={ t.throwable.get.getMessage } type={ t.throwable.getClass.getName }>{ t.throwable.get.getStackTrace.map { e => e.toString }.mkString("\n") }</failure>
+                  case TestResult.Error =>
+                    <error message={ t.throwable.get.getMessage } type={ t.throwable.getClass.getName }>{ t.throwable.get.getStackTrace.map { e => e.toString }.mkString("\n") }</error>
                   case _ => {}
                 }
               }
@@ -76,7 +69,7 @@ class TestGroupXmlWriter(val name: String) {
         <system-err></system-err>
       </testsuite>
 
-    XML.save(path + "TEST-" + name + ".xml",resultXml,xmlDecl = true)
+    XML.save(path + "TEST-" + name + ".xml", resultXml, xmlDecl = true)
 
   }
 
